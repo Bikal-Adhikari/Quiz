4adaef944d4b2ea0e636d3fe7ebf24b41c8afd0e
Bikal <bikaladhikari5@gmail.com>
2024-04-01 23:19:13 +1100

    answer shuffeling function

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,6 +4,8 @@ import Questions from "../questions.js";
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const shuffledAnswers = [...Questions[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
   const handelSelectAnswer = (selectedAnswers) => {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswers];
@@ -14,7 +16,7 @@ const Quiz = () => {
       <div id="question">
         <p>{Questions[activeQuestionIndex].text}</p>
         <ul id="answers">
-          {Questions[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handelSelectAnswer(answer)}>
                 {answer}
