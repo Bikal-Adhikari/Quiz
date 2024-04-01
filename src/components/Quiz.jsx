092801e54bfb77a8dcf005c39675ec8effc56239
Bikal <bikaladhikari5@gmail.com>
2024-04-01 23:15:10 +1100

    Quiz components added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
+import Questions from "../questions.js";
 
 const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+  const handelSelectAnswer = (selectedAnswers) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswers];
+    });
+  };
   return (
-    <div>Quiz</div>
-  )
-}
+    <div id="quiz">
+      <div id="question">
+        <p>{Questions[activeQuestionIndex].text}</p>
+        <ul id="answers">
+          {Questions[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handelSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Quiz
+export default Quiz;
