f558692e2cf390ab2c24f26b85bfae11774351b9
Bikal <bikaladhikari5@gmail.com>
2024-04-01 23:24:56 +1100

    Condition added for after completion

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import Questions from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-  const shuffledAnswers = [...Questions[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  const quizISComplete = activeQuestionIndex === Questions.length;
   const handelSelectAnswer = (selectedAnswers) => {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswers];
     });
   };
+
+  if (quizISComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+  const shuffledAnswers = [...Questions[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
       <div id="question">
